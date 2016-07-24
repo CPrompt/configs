fe18b6e39274e110ab7b414a3d9f6fbada367eae
Curtis Adkins <curtis@wayne-manor.gotham>
2016-07-24 11:23:31 -0400

    changed the i3 config file to launch keepassx and thunar and send straight to scratchpad

--- .i3/i3pystatus/config_monitor1.py
+++ .i3/i3pystatus/config_monitor1.py
@@ -4,15 +4,12 @@ from i3pystatus.weather import weathercom
 
 import subprocess
 
-color_teal = "#458588"
-
 status = Status(standalone=True)
-
 
 # Uses weather.com to get current temp
 
 status.register("weather",
-	format='{city} : {condition} {current_temp}{temp_unit} {icon} Lo: {low_temp}',
+        format='{city} : Right Now: {condition} {current_temp}{temp_unit} {icon}   Hi: {high_temp} Lo: {low_temp}',
 	colorize=True,
 	color_icons = {
         'Fair': (u'\u263c', '#ffcc00'),
@@ -34,7 +31,7 @@ status.register("weather",
 status.register("pulseaudio",
         color_unmuted = "#458588",
 	format = ' : {volume}',)
-'''
+
 status.register("cmus",
     color = '#458588',
     #color = '#00ff00',
@@ -42,17 +39,22 @@ status.register("cmus",
     #color_not_running = '#ffffff',
     format = '{status} {song_elapsed}/{song_length} {artist} - {title}',
     format_not_running = 'Not running',
+    status = {
+        'paused': '',
+        'playing': '',
+        'stopped': '',
+    },
     interval = 1,
     )
-
+'''
 status.register("now_playing",)
 '''
-
+'''
 status.register("mpd",
         #color = '#458588',
-        color = color_teal,
+        color = '#458588',
 	status = {"pause": "","play": "","stop": "",},
         format = '{status} {song_elapsed}/{len} {artist} - {title}',
         )
-
+'''
 status.run()
