207d8219c042ebf363d62395c263654ce07c8e30
Curtis Adkins <curtis@wayne-manor.gotham>
2016-11-27 16:48:10 -0500

    Some serious cleanup and screwups

--- .i3/load_layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
-#fill the containers
-(Firefox &)
-(urxvt256c &)
-(Thunderbird &)
--- .i3/load_workspac4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-i3-msg "workspace 4; append_layout ~/.i3/workspace-4.json"
-#fill the containers
-(thunar "/home/curtis/" &)
-(keepassx -lock &)
