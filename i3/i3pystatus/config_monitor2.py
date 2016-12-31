ffd6b63afc9964c95d4f3623a977a5fc1e89fa2b
Curtis Adkins <curtis@wayne-manor.gotham>
2016-12-31 13:25:52 -0500

    Modifying the i3 to include i3-container-commander https://github.com/acrisci/i3ipc-python

--- i3/i3pystatus/config_monitor2.py
+++ i3/i3pystatus/config_monitor2.py
@@ -11,14 +11,14 @@ from colors import *
 
 # Displays clock like this:
 status.register("clock",
-#  Note: this config requires the gsimplecal package to be installed
-    hints = {"markup": "pango"},
-    format = " %a %d %b <span color=\"#ebdbb2\">  </span>  %H:%M:%S %P <span color=\"#ebdbb2\"> </span> ",
-#    format = "(' %a %d %b ', America/New_York)",
-    #color = "#b16286",
-    #color = "#5f87af",
-    color = color_clock,
-    on_leftclick = ["gsimplecal"],)
+                        #  Note: this config requires the gsimplecal package to be installed
+                        hints = {"markup": "pango"},
+                        format = " %a %d %b <span color=\"#ebdbb2\">  </span>  %H:%M:%S %P <span color=\"#ebdbb2\"> </span> ",
+                        #    format = "(' %a %d %b ', America/New_York)",
+                        #color = "#b16286",
+                        #color = "#5f87af",
+                        color = color_clock,
+                        on_leftclick = ["gsimplecal"],)
 
 # Display updates
 
@@ -31,6 +31,7 @@ status.register("updates",
                         color_working = None,
                         format_no_updates = "No updates",
                         format_working = "Working...",
+                        notification_icon = "software-update-available",
                         backends = [dnf.Dnf()],)
 
 # Shows disk usage of /home/curtis/
@@ -46,13 +47,13 @@ status.register("disk",
 
 # Shows internet connection status
 status.register("online",
-    format_online = '',
-    #color = '#98971a',
-    #color = '#00DD00',
-    color = color_online,
-    format_offline = '',
-    #color_offline = '#FF0000',)
-    color_offline = color_offline,)
+                        format_online = '',
+                        #color = '#98971a',
+                        #color = '#00DD00',
+                        color = color_online,
+                        format_offline = '',
+                        #color_offline = '#FF0000',)
+                        color_offline = color_offline,)
 
 
 # Shows the address and up/down state of eth0. If it is up the address is shown in
@@ -64,15 +65,14 @@ status.register("online",
 # Note: the network module requires PyPI package netifaces
 
 status.register("network",
-    interface="enp2s0",
-    hints = {"markup": "pango"},
-    format_up="<span color=\"#ebdbb2\"> :</span> {v4} ",
-    #color_up = "#98971a",
-    #color_up = "#fd971f",
-    #color_down = "red",)
-    color_up = color_network_up,
-    color_down = color_network_down,
-    )
+                        interface="enp2s0",
+                        hints = {"markup": "pango"},
+                        format_up="<span color=\"#ebdbb2\"> :</span> {v4} ",
+                        #color_up = "#98971a",
+                        #color_up = "#fd971f",
+                        #color_down = "red",)
+                        color_up = color_network_up,
+                        color_down = color_network_down,)
 
 
 
