906d077c0926b5bc07e3e91cbbf2c77c56544e2e
Curtis Adkins <curtis@wayne-manor.gotham>
2018-08-23 06:04:46 -0400

    Change to i3pystatus weather.com module that errored if "imperial" was set as unit.

--- i3/i3pystatus/config_monitor1.py
+++ i3/i3pystatus/config_monitor1.py
@@ -9,11 +9,12 @@ from colors import *
 
 status = Status(
         logfile='/home/curtis/.config/i3/i3pystatus/i3pystatus.log',
-        logformat='%(asctime)s $(levelname)s:',
+        #logformat='%(asctime)s $(levelname)s:',
         standalone=True,
         )
 
 # Uses weather.com to get current temp
+
 status.register(
         'weather',
         format='{city}    {condition}  {current_temp}{temp_unit}{icon}   Hi: {high_temp} Lo: {low_temp}',
@@ -31,38 +32,38 @@ status.register(
         interval = 1000,
 	backend=weathercom.Weathercom(
             location_code="USNC0314:1:US",
-            units='imperial',
-            #update_error='<span color="#ff0000">|</span>',
-            #log_level=10,
+            #units='imperial',
+            update_error='<span color="#ff0000">|</span>',
+            log_level=10,
         ),
 )
-
+'''
 
 #http://api.wunderground.com/api/0b012a21f26a3f71/conditions/q/NC/pws:KNCHIGHP24.json
 
 
-#status.register(
-#            'weather',
-#            format='{city} {condition} {current_temp}{temp_unit}{icon}  Hi: {high_temp} Lo: {low_temp}',
-#            colorize=True,
-#            color_icons = {
-#				'Fair': (u'\u263c', '#ffcc00'),
-#				'Cloudy': (u'\u2601', '#f8f8ff'),
-#				'Partly Cloudy': (u'\u2601', '#f8f8ff'),  # \u26c5 is not in many fonts
-#				'Rainy': (u'\u26c8', '#cbd2c0'),
-#				'Thunderstorm': (u'\u03de', '#cbd2c0'),
-#				'Sunny': (u'\u2600', '#ffff00'),
-#				'Snow': (u'\u2603', '#ffffff'),
-#				'default': ('', None),
-#			},
-#           backend=wunderground.Wunderground(
-#                api_key='0b012a21f26a3f71',
-#                location_code='pws:KNCHIGHP24',
-#				units='imperial',
-#				forecast='True',
-#	),
-#)
-
+status.register(
+            'weather',
+            format='{city} {condition} {current_temp}{temp_unit}{icon}  Hi: {high_temp} Lo: {low_temp}',
+            colorize=True,
+            color_icons = {
+	        'Fair': (u'\u263c', '#ffcc00'),
+		'Cloudy': (u'\u2601', '#f8f8ff'),
+		'Partly Cloudy': (u'\u2601', '#f8f8ff'),  # \u26c5 is not in many fonts
+		'Rainy': (u'\u26c8', '#cbd2c0'),
+		'Thunderstorm': (u'\u03de', '#cbd2c0'),
+		'Sunny': (u'\u2600', '#ffff00'),
+		'Snow': (u'\u2603', '#ffffff'),
+		'default': ('', None),
+			},
+            backend=wunderground.Wunderground(
+                api_key='0b012a21f26a3f71',
+                location_code='pws:KNCHIGHP24',
+	        units='imperial',
+	        forecast='True',
+	    ),
+)
+'''
 
 status.register("pulseaudio",
         color_unmuted = pulse_audio,
@@ -84,7 +85,7 @@ status.register("pulseaudio",
 #			'stop': '',
 #		},
 #       format = '{song_elapsed}/{song_length} {artist} - {title}  {status}',
-#		format_no_player = 'Not running',		
+#		format_no_player = 'Not running',
 #       )
 
 status.register("cmus",
